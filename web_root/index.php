4e7f5dd8aac51938fc84bb20e953769c94ec25e0
Gabriel Afton <gabrielafton@gmail.com>
2013-08-24 15:29:43 -0400

    Updated Zend structure. Minor mods to UI.

--- web_root/index.php
+++
@@ -1,9 +0,0 @@
-1
-<?php
-error_reporting(E_ALL|E_STRICT);
-ini_set('display_errors', false);
-$rootDir = dirname(dirname(__FILE__));
-set_include_path($rootDir . '/library/zf2.2.2' . PATH_SEPARATOR . get_include_path());
-require_once 'Controller/Front.php';
-Zend_Controller_Front::run('application/controllers');
-?>
