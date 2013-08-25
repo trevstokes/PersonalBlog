a5a3f53b7fbfec08ab93b49519346a4f793509ed
Gabriel Afton <gabrielafton@gmail.com>
2013-08-25 01:54:36 -0400

    Code fix and UI changes
    Fixed code causing index page to error out.
    Updated blog post comment design to reflect BS3.0 form styles.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -10,7 +10,7 @@
 return array(
     'router' => array(
         'routes' => array(
-            'home' => array(
+            'index' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
                     'route'    => '/',
