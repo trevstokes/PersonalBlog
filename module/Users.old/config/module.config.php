a5068f5b2ce406e55edadf7849a5f7aeb311b859
Gabriel Afton <gabrielafton@gmail.com>
2013-08-25 21:11:01 -0400

    Updates

---
+++ module/Users.old/config/module.config.php
@@ -0,0 +1,34 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Users\Controller\Users' => 'Users\Controller\UsersController',
+        ),
+    ),
+
+    'router' => array(
+        'routes' => array(
+            'users' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/users[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Users\Controller\Users',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+	
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'users' => __DIR__ . '/../view',
+        ),
+    ),
+);
+?>
